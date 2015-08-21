4eb1b0d77607ebc78741800574db70c605ad1d95
NEMESIS13cz <seke94@seznam.cz>
2015-08-21 16:51:55 +0200

    [Optimiziation] Changed color handling

--- com/redstoner/nemes/t3tris/world/Block.java
+++ com/redstoner/nemes/t3tris/world/Block.java
@@ -1,26 +1,47 @@
 package com.redstoner.nemes.t3tris.world;
 
+import java.util.Random;
+
 import org.lwjgl.util.Color;
 
 import com.redstoner.nemes.t3tris.Render;
 
 public class Block {
 
-	private Color colour = null;
+	private float r, g, b, a;
 	
 	public Block(Color c) {
-		colour = c;
+		r = c.getRed() / 255.0f;
+		g = c.getGreen() / 255.0f;
+		b = c.getBlue() / 255.0f;
+		a = c.getAlpha() / 255.0f;
 	}
 	
 	public Color getColour() {
-		return colour;
+		return new Color((int)(r * 255), (int)(g * 255), (int)(b * 255), (int)(a * 255));
 	}
 	
-	public void update(Grid g, int x, int y, int z) {
+	public float getRed() {
+		return r;
+	}
+	
+	public float getGreen() {
+		return g;
+	}
+	
+	public float getBlue() {
+		return b;
+	}
+	
+	public float getAlpha() {
+		return a;
+	}
+	
+	public void update(Grid g, int x, int y, int z, Random rand) {
 		
 	}
 	
-	public void render(Grid g, Render r, int x, int y, int z) {
+	public void render(Grid g, Render r, int x, int y, int z, Random rand) {
 		
 	}
 	
